2cf9bc486349613cb36dee97ad70ae5bdbf54239
Pogoparis <pogoparis@gmail.com>
2026-05-26 04:20:23 +0200

    Simplify enemy architecture and remove unused pivot system

--- scripts/enemies/enemy_base.gd
+++ scripts/enemies/enemy_base.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 class_name EnemyBase
 
 @export var max_hp := 30
@@ -7,37 +7,41 @@ class_name EnemyBase
 
 var hp := 0
 
+@onready var hp_fill = $HPBarContainer/HPFill
+
 func _ready():
+
 	hp = max_hp
+
+	update_hp_bar()
 
 func take_damage(amount: int):
 
 	hp -= amount
-	queue_redraw()
+
+	if hp < 0:
+		hp = 0
+
+	update_hp_bar()
+
 	if hp <= 0:
 		die()
+
+func update_hp_bar():
+
+	var ratio = float(hp) / float(max_hp)
+
+	hp_fill.size.x = 40 * ratio
+
+	if ratio > 0.6:
+		hp_fill.color = Color(0.2, 1, 0.2)
+	elif ratio > 0.3:
+		hp_fill.color = Color(1, 0.7, 0.2)
+	else:
+		hp_fill.color = Color(1, 0.2, 0.2)
 
 func die():
 
 	EconomyManager.add_scrap(scrap_reward)
 
 	queue_free()
-
-func _draw():
-
-	var width := 32.0
-	var height := 5.0
-
-	var ratio = float(hp) / float(max_hp)
-
-	# fond rouge
-	draw_rect(
-		Rect2(-width * 0.5, -40, width, height),
-		Color(0.4, 0, 0)
-	)
-
-	# vie verte
-	draw_rect(
-		Rect2(-width * 0.5, -40, width * ratio, height),
-		Color(0, 1, 0)
-	)
