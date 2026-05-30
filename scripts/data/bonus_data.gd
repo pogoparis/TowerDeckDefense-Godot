c9212d6b1d978060f6a67bd2592b358bde0f4709
Pogoparis <pogoparis@gmail.com>
2026-05-30 21:25:35 +0200

    Add reward bonus system foundation and rarity support

--- scripts/data/bonus_data.gd
+++ scripts/data/bonus_data.gd
@@ -1,9 +1,16 @@
 extends Resource
 class_name BonusData
 
-@export var title: String
-@export_multiline var description: String
+@export var title : String
+@export_multiline var description : String
 
 @export var damage_bonus := 0
+@export var range_bonus := 0
 @export var fire_rate_mult := 1.0
-@export var range_bonus := 0.0
+
+@export_enum("COMMON", "RARE", "EPIC", "LEGENDARY")
+var rarity : String = "COMMON"
+
+@export var slow_bonus := 0.0
+@export var burn_bonus := 0.0
+@export var capsule_bonus := 0.0
